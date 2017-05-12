a584af89cd80ead9531dec02442157690d1e7ca6
Martin Bauer <bauer_martin@icloud.com>
2017-05-12 12:01:18 +0200

    Extract semaphore implementation detail

--- Cinema/TMDBSwiftWrapper.swift
+++ Cinema/TMDBSwiftWrapper.swift
@@ -19,78 +19,76 @@ class TMDBSwiftWrapper: MovieDbClient {
   private(set) var isConnected: Bool = false
 
   func poster(for id: Int, size: PosterSize) -> UIKit.UIImage? {
-    if Thread.isMainThread {
-      fatalError("must not be called on the main thread")
-    }
     var value: UIKit.UIImage?
-    let semaphore = DispatchSemaphore(value: 0)
-    MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
-      apiReturn, movie in
-      if let posterPath = movie?.poster_path {
-        let path = TMDBSwiftWrapper.baseUrl + size.rawValue + posterPath
-        let image = try! UIImage(data: Data(contentsOf: URL(string: path)!))
-        value = image
+    waitUntil { done in
+      MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
+        apiReturn, movie in
+        if let posterPath = movie?.poster_path {
+          let path = TMDBSwiftWrapper.baseUrl + size.rawValue + posterPath
+          let image = try! UIImage(data: Data(contentsOf: URL(string: path)!))
+          value = image
+        }
+        done()
       }
-      semaphore.signal()
     }
-    semaphore.wait()
     return value
   }
 
   func overview(for id: Int) -> String? {
-    if Thread.isMainThread {
-      fatalError("must not be called on the main thread")
-    }
     var value: String?
-    let semaphore = DispatchSemaphore(value: 0)
-    MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
-      apiReturn, movie in
-      value = movie?.overview
-      semaphore.signal()
+    waitUntil { done in
+      MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
+        apiReturn, movie in
+        value = movie?.overview
+        done()
+      }
     }
-    semaphore.wait()
     return value
   }
 
   func certification(for id: Int) -> String? {
-    if Thread.isMainThread {
-      fatalError("must not be called on the main thread")
-    }
     var value: String?
-    let semaphore = DispatchSemaphore(value: 0)
-    MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
-      apiReturn, movie in
-      MovieMDB.release_dates(TMDBSwiftWrapper.apiKey, movieID: id) {
-        apiReturn, releaseDates in
-        if let releaseDates = releaseDates {
-          for date in releaseDates {
-            if date.iso_3166_1 == TMDBSwiftWrapper.country {
-              value = date.release_dates[0].certification
+    waitUntil { done in
+      MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
+        apiReturn, movie in
+        MovieMDB.release_dates(TMDBSwiftWrapper.apiKey, movieID: id) {
+          apiReturn, releaseDates in
+          if let releaseDates = releaseDates {
+            for date in releaseDates {
+              if date.iso_3166_1 == TMDBSwiftWrapper.country {
+                value = date.release_dates[0].certification
+              }
             }
           }
+          done()
         }
-        semaphore.signal()
       }
     }
-    semaphore.wait()
     return value
   }
 
   func genres(for id: Int) -> [String] {
+    var value = [String]()
+    waitUntil { done in
+      MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
+        apiReturn, movie in
+        if let genres = movie?.genres.map({ $0.name! }) {
+          value = genres
+        }
+        done()
+      }
+    }
+    return value
+  }
+
+  private func waitUntil(_ asyncProcess: (_ done: @escaping () -> Void) -> Void) {
     if Thread.isMainThread {
       fatalError("must not be called on the main thread")
     }
-    var value = [String]()
     let semaphore = DispatchSemaphore(value: 0)
-    MovieMDB.movie(TMDBSwiftWrapper.apiKey, movieID: id, language: TMDBSwiftWrapper.language) {
-      apiReturn, movie in
-      if let genres = movie?.genres.map({ $0.name! }) {
-        value = genres
-      }
-      semaphore.signal()
-    }
+    let done = { _ = semaphore.signal() }
+    asyncProcess(done)
     semaphore.wait()
-    return value
   }
 
 }
