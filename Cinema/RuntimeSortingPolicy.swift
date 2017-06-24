27b5773bcf9fc9d343054f1a0596c5fc9f988a6d
Martin Bauer <bauer_martin@icloud.com>
2017-06-24 13:33:58 +0200

    Fix sorting for items that have no runtime
    Closes #1

--- Cinema/RuntimeSortingPolicy.swift
+++ Cinema/RuntimeSortingPolicy.swift
@@ -2,29 +2,43 @@ import Foundation
 
 struct RuntimeSortingPolicy: SortingPolicy {
 
+  private let unknownSymbol = "?"
+
   func sectionIndexTitle(for item: MediaItem) -> String {
-    return String(item.runtime / 10 * 10)
+    switch item.runtime {
+      case -1: return unknownSymbol
+      default: return String(item.runtime / 10 * 10)
+    }
   }
 
   func refineSectionIndexTitles(_ sections: [String]) -> [String] {
+    guard !sections.isEmpty && sections != [unknownSymbol] else { return [] }
+    let containsUnknownElements = sections.last! == unknownSymbol
     let min = Int(sections.first!)!
-    let max = Int(sections.last!)!
+    let maxIndex = sections.count - 1 - (containsUnknownElements ? 1 : 0)
+    let max = Int(sections[maxIndex])!
     if min == max {
-      return sections
+      return Array(sections.dropLast())
     } else {
       return Array(stride(from: min, through: max, by: 10)).map({ String($0) })
     }
   }
 
   func sectionTitle(for sectionIndexTitle: String) -> String {
-    let runtime = DateComponents(minute: Int(sectionIndexTitle)!)
-    let formatter = DateComponentsFormatter()
-    formatter.unitsStyle = .full
-    formatter.allowedUnits = [.minute]
-    return formatter.string(from: runtime)!
+    switch sectionIndexTitle {
+      case unknownSymbol: return NSLocalizedString("sort.by.runtime.unknownHeader", comment: "")
+      default:
+        let runtime = DateComponents(minute: Int(sectionIndexTitle)!)
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .full
+        formatter.allowedUnits = [.minute]
+        return formatter.string(from: runtime)!
+    }
   }
 
   func sectionIndexTitleSorting(left: String, right: String) -> Bool {
+    guard left != unknownSymbol else { return false }
+    guard right != unknownSymbol else { return true }
     return Int(left)! <= Int(right)!
   }
 
