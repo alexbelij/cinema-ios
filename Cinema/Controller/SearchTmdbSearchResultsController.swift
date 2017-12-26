13d2788fc2ec16c5e8aa71cb52e48fa26d383e34
Martin Bauer <bauer_martin@icloud.com>
2017-12-26 16:53:19 +0100

    Make SearchTmdbSearchResultsController an UITableViewController

--- Cinema/Controller/SearchTmdbSearchResultsController.swift
+++ Cinema/Controller/SearchTmdbSearchResultsController.swift
@@ -1,10 +1,7 @@
 import UIKit
 
-class SearchTmdbSearchResultsController: UIViewController {
-  @IBOutlet private weak var tableView: UITableView!
+class SearchTmdbSearchResultsController: UITableViewController {
   private lazy var emptyView = GenericEmptyView()
-  private var previousTableViewInsets: UIEdgeInsets?
-
   var searchText: String = "" {
     didSet {
       self.emptyView.configure(
@@ -27,31 +24,16 @@ class SearchTmdbSearchResultsController: UIViewController {
     }
   }
   var selectionHandler: ((SearchTmdbController.SearchResult) -> Void)?
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    tableView.delegate = self
-    tableView.dataSource = self
-    let notificationCenter = NotificationCenter.default
-    notificationCenter.addObserver(self,
-                                   selector: #selector(keyboardDidShow(_:)),
-                                   name: .UIKeyboardDidShow,
-                                   object: nil)
-    notificationCenter.addObserver(self,
-                                   selector: #selector(keyboardWillHide(_:)),
-                                   name: .UIKeyboardWillHide,
-                                   object: nil)
-  }
 }
 
 // MARK: - Table View
 
-extension SearchTmdbSearchResultsController: UITableViewDataSource, UITableViewDelegate {
-  public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+extension SearchTmdbSearchResultsController {
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return searchResults.count
   }
 
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let searchResult = searchResults[indexPath.row]
     if searchResult.hasBeenAddedToLibrary {
       let cell = tableView.dequeueReusableCell(withIdentifier: "SearchItemAddedCell",
@@ -67,42 +49,17 @@ extension SearchTmdbSearchResultsController: UITableViewDataSource, UITableViewD
     }
   }
 
-  func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 44
+  }
+
+  override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
     return searchResults[indexPath.row].hasBeenAddedToLibrary ? nil : indexPath
   }
 
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     selectionHandler?(searchResults[indexPath.row])
     tableView.deselectRow(at: indexPath, animated: true)
-  }
-}
-
-// MARK: - Keyboard Adjustments
-
-extension SearchTmdbSearchResultsController {
-  @objc
-  private func keyboardDidShow(_ notification: Notification) {
-    guard let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? CGRect else { return }
-    var contentInsets: UIEdgeInsets
-    if previousTableViewInsets == nil {
-      contentInsets = tableView.contentInset
-      self.previousTableViewInsets = contentInsets
-    } else {
-      contentInsets = tableView.contentInset
-    }
-    contentInsets.bottom = keyboardFrame.height - view.safeAreaInsets.bottom
-    self.tableView.contentInset = contentInsets
-    self.tableView.scrollIndicatorInsets = contentInsets
-  }
-
-  @objc
-  private func keyboardWillHide(_ notification: Notification) {
-    guard let insets = self.previousTableViewInsets else { return }
-    UIView.animate(withDuration: 0.3) {
-      self.tableView.contentInset = insets
-      self.tableView.scrollIndicatorInsets = insets
-    }
-    self.previousTableViewInsets = nil
   }
 }
 
