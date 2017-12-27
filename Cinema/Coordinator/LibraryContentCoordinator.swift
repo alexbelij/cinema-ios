82305d38c2bb3be992828964a1fb88a1c3c0d641
Martin Bauer <bauer_martin@icloud.com>
2017-12-27 11:55:53 +0100

    Update detail item on external update

--- Cinema/Coordinator/LibraryContentCoordinator.swift
+++ Cinema/Coordinator/LibraryContentCoordinator.swift
@@ -142,6 +142,12 @@ extension LibraryContentCoordinator: MediaLibraryDelegate {
         movieListItems.remove(at: index)
         movieListItems.insert(item, at: index)
       }
+      DispatchQueue.main.async {
+        if let itemDetailsCoordinator = self.itemDetailsCoordinator,
+           let updatedDetailItem = contentUpdate.updatedItems[itemDetailsCoordinator.detailItem.id] {
+          self.itemDetailsCoordinator!.updateNonRemoteProperties(with: updatedDetailItem)
+        }
+      }
     }
 
     // new movies
