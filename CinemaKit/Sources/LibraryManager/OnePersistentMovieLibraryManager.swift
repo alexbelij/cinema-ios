95a94e53af6cf8b464a4c98d6e731ad4bb4fc396
Martin Bauer <bauer_martin@icloud.com>
2018-08-15 09:35:17 +0200

    Restructure startup process

--- CinemaKit/Sources/LibraryManager/OnePersistentMovieLibraryManager.swift
+++ CinemaKit/Sources/LibraryManager/OnePersistentMovieLibraryManager.swift
@@ -2,38 +2,15 @@ import Foundation
 import os.log
 
 public class OnePersistentMovieLibraryManager: MovieLibraryManager {
-  private static let logger = Logging.createLogger(category: "LibraryManager")
   public weak var delegate: MovieLibraryManagerDelegate?
   private var libraries: [UUID: InternalMovieLibrary]
 
-  public init() {
+  public init(url: URL) {
     let metadata = MovieLibraryMetadata(name: NSLocalizedString("library", comment: ""))
-    let url = directoryUrl(for: .documentDirectory).appendingPathComponent("cinema.data")
-    OnePersistentMovieLibraryManager.moveLegacyLibraryFile(to: url)
     let dataFormat = KeyedArchivalFormat()
     dataFormat.defaultSchemaVersion = .v2_0_0
     let library = FileBasedMovieLibrary(metadata: metadata, url: url, dataFormat: dataFormat)
     libraries = [library.metadata.id: library]
-  }
-
-  private static func moveLegacyLibraryFile(to url: URL) {
-    let legacyUrl = directoryUrl(for: .applicationSupportDirectory, createIfNecessary: false)
-        .appendingPathComponent(Bundle.main.bundleIdentifier!, isDirectory: true)
-        .appendingPathComponent("cinema.data")
-    if FileManager.default.fileExists(atPath: legacyUrl.path) {
-      do {
-        try FileManager.default.moveItem(at: legacyUrl, to: url)
-        os_log("moved legacy library data file from 'Application Support' to 'Documents'",
-               log: OnePersistentMovieLibraryManager.logger,
-               type: .default)
-      } catch {
-        os_log("unable to move legacy library data file: %{public}@",
-               log: OnePersistentMovieLibraryManager.logger,
-               type: .fault,
-               String(describing: error))
-        fatalError("unable to move legacy library data file")
-      }
-    }
   }
 
   public var libraryCount: Int {
@@ -67,20 +44,4 @@ public class OnePersistentMovieLibraryManager: MovieLibraryManager {
     delegate?.libraryManager(self, didRemove: library)
     completion(.success(()))
   }
-}
-
-private func directoryUrl(for directory: FileManager.SearchPathDirectory,
-                          createIfNecessary: Bool = true) -> URL {
-  let fileManager = FileManager.default
-  let dir = fileManager.urls(for: directory, in: .userDomainMask).first!
-  do {
-    var isDirectory: ObjCBool = false
-    if !(FileManager.default.fileExists(atPath: dir.path, isDirectory: &isDirectory)
-         && isDirectory.boolValue) {
-      try FileManager.default.createDirectory(at: dir, withIntermediateDirectories: true, attributes: nil)
-    }
-  } catch {
-    fatalError("Could not create \(dir)")
-  }
-  return dir
 }
--- CinemaKit/Sources/StartupManager.swift
+++ CinemaKit/Sources/StartupManager.swift
@@ -8,18 +8,69 @@ public protocol StartupManager {
 public class CinemaKitStartupManager: StartupManager {
   private static let logger = Logging.createLogger(category: "CinemaKitStartupManager")
 
+  // directories
+  private static let documentsDir = directoryUrl(for: .documentDirectory)
+  private static let appSupportDir = directoryUrl(for: .applicationSupportDirectory)
+      .appendingPathComponent(Bundle.main.bundleIdentifier!, isDirectory: true)
+
+  // cinema data file
+  private static let libraryDataFileURL = documentsDir.appendingPathComponent("cinema.data")
+  private static let legacyLibraryDataFileURL = appSupportDir.appendingPathComponent("cinema.data")
+
   public init() {
   }
 
   public func initialize(then completion: @escaping (AppDependencies) -> Void) {
-    completion(makeDependencies())
+    os_log("initializing CinemaKit", log: CinemaKitStartupManager.logger, type: .default)
+    setUpDirectories()
+    moveLegacyLibraryFile()
+    let dependencies = makeDependencies()
+    os_log("finished initializing CinemaKit", log: CinemaKitStartupManager.logger, type: .default)
+    completion(dependencies)
+  }
+
+  private func setUpDirectories() {
+    os_log("setting up directories", log: CinemaKitStartupManager.logger, type: .info)
+    makeDirectory(at: CinemaKitStartupManager.documentsDir)
+  }
+
+  private func makeDirectory(at url: URL) {
+    if FileManager.default.fileExists(atPath: url.path) { return }
+    do {
+      try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true)
+    } catch {
+      os_log("unable to create directory at %{public}@: %{public}@",
+             log: CinemaKitStartupManager.logger,
+             type: .fault,
+             url.path,
+             String(describing: error))
+    }
+  }
+
+  private func moveLegacyLibraryFile() {
+    os_log("searching for legacy library file", log: CinemaKitStartupManager.logger, type: .info)
+    if FileManager.default.fileExists(atPath: CinemaKitStartupManager.legacyLibraryDataFileURL.path) {
+      do {
+        os_log("moving legacy library data file from 'Application Support' to 'Documents'",
+               log: CinemaKitStartupManager.logger,
+               type: .default)
+        try FileManager.default.moveItem(at: CinemaKitStartupManager.legacyLibraryDataFileURL,
+                                         to: CinemaKitStartupManager.libraryDataFileURL)
+      } catch {
+        os_log("unable to move legacy library data file: %{public}@",
+               log: CinemaKitStartupManager.logger,
+               type: .fault,
+               String(describing: error))
+        fatalError("unable to move legacy library data file")
+      }
+    }
   }
 
   private func makeDependencies() -> AppDependencies {
-    os_log("gathering dependencies", log: CinemaKitStartupManager.logger, type: .default)
+    os_log("gathering dependencies", log: CinemaKitStartupManager.logger, type: .info)
 
     // Library Manager
-    let libraryManager = OnePersistentMovieLibraryManager()
+    let libraryManager = OnePersistentMovieLibraryManager(url: CinemaKitStartupManager.libraryDataFileURL)
 
     // MovieDb Client
     let language = MovieDbLanguage(rawValue: Locale.current.languageCode ?? "en") ?? .en
@@ -28,4 +79,8 @@ public class CinemaKitStartupManager: StartupManager {
 
     return AppDependencies(libraryManager: libraryManager, movieDb: movieDb)
   }
+}
+
+private func directoryUrl(for directory: FileManager.SearchPathDirectory) -> URL {
+  return FileManager.default.urls(for: directory, in: .userDomainMask).first!
 }
