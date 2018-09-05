936ea7e739ce88ccd9d604d7ea52b3a3a8a17e03
Martin Bauer <bauer_martin@icloud.com>
2018-09-05 15:50:36 +0200

    Cosmetics

--- iOS/Application/Sources/LibraryList/LibraryListController.swift
+++ iOS/Application/Sources/LibraryList/LibraryListController.swift
@@ -3,13 +3,13 @@ import Dispatch
 import UIKit
 
 class LibraryListController: UITableViewController {
-  private enum ListItem {
+  private enum LibraryItem {
     case placeholder(MovieLibraryMetadata)
     case selectLibrary(MovieLibraryMetadata)
     case addLibraryAction
   }
 
-  private static let sortDescriptor: (ListItem, ListItem) -> Bool = { item1, item2 in
+  private static let sortDescriptor: (LibraryItem, LibraryItem) -> Bool = { item1, item2 in
     switch (item1, item2) {
       case let (.placeholder(metadata1), .placeholder(metadata2)):
         return StandardSortDescriptors.byMetadataName(metadata1, metadata2)
@@ -27,30 +27,30 @@ class LibraryListController: UITableViewController {
   var onSelection: ((MovieLibraryMetadata) -> Void)?
   var onAddLibraryButtonTap: (() -> Void)?
 
-  private var viewModel = [ListItem.addLibraryAction]
+  private var libraryItems = [LibraryItem.addLibraryAction]
 }
 
 // MARK: - Setup
 
 extension LibraryListController {
   func setLibraries(_ data: [MovieLibraryMetadata]) {
-    viewModel = data.map { ListItem.selectLibrary($0) } + [.addLibraryAction]
-    viewModel.sort(by: LibraryListController.sortDescriptor)
+    libraryItems = data.map { LibraryItem.selectLibrary($0) } + [.addLibraryAction]
+    libraryItems.sort(by: LibraryListController.sortDescriptor)
     tableView?.reloadData()
   }
 
   func showPlaceholder(for metadata: MovieLibraryMetadata) {
-    showItem(.placeholder(metadata), with: metadata)
+    showLibraryItem(.placeholder(metadata), with: metadata)
   }
 
   func showLibrary(with metadata: MovieLibraryMetadata) {
-    showItem(.selectLibrary(metadata), with: metadata)
+    showLibraryItem(.selectLibrary(metadata), with: metadata)
   }
 
-  private func showItem(_ newItem: ListItem, with metadata: MovieLibraryMetadata) {
-    if let oldIndex = viewModelIndex(for: metadata) {
+  private func showLibraryItem(_ newItem: LibraryItem, with metadata: MovieLibraryMetadata) {
+    if let oldIndex = libraryItemIndex(for: metadata) {
       let shouldUpdate: Bool
-      switch (viewModel[oldIndex], newItem) {
+      switch (libraryItems[oldIndex], newItem) {
         case let (.placeholder(oldMetadata), .placeholder),
              let (.selectLibrary(oldMetadata), .selectLibrary):
           shouldUpdate = oldMetadata != metadata
@@ -58,31 +58,31 @@ extension LibraryListController {
           shouldUpdate = true
       }
       if shouldUpdate {
-        viewModel.remove(at: oldIndex)
-        viewModel.append(newItem)
-        viewModel.sort(by: LibraryListController.sortDescriptor)
-        let newIndex = viewModelIndex(for: metadata)!
+        libraryItems.remove(at: oldIndex)
+        libraryItems.append(newItem)
+        libraryItems.sort(by: LibraryListController.sortDescriptor)
+        let newIndex = libraryItemIndex(for: metadata)!
         if oldIndex != newIndex {
           tableView.moveRow(at: IndexPath(row: oldIndex, section: 0), to: IndexPath(row: newIndex, section: 0))
         }
         tableView.reloadRows(at: [IndexPath(row: newIndex, section: 0)], with: .automatic)
       }
     } else {
-      viewModel.append(newItem)
-      viewModel.sort(by: LibraryListController.sortDescriptor)
-      let newIndex = viewModelIndex(for: metadata)!
+      libraryItems.append(newItem)
+      libraryItems.sort(by: LibraryListController.sortDescriptor)
+      let newIndex = libraryItemIndex(for: metadata)!
       tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .automatic)
     }
   }
 
   func removeItem(for metadata: MovieLibraryMetadata) {
-    guard let index = viewModelIndex(for: metadata) else { return }
-    viewModel.remove(at: index)
+    guard let index = libraryItemIndex(for: metadata) else { return }
+    libraryItems.remove(at: index)
     tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
   }
 
-  private func viewModelIndex(for metadata: MovieLibraryMetadata) -> Int? {
-    return viewModel.index {
+  private func libraryItemIndex(for metadata: MovieLibraryMetadata) -> Int? {
+    return libraryItems.index {
       switch $0 {
         case let .placeholder(metadata1): return metadata1.id == metadata.id
         case let .selectLibrary(metadata1): return metadata1.id == metadata.id
@@ -96,45 +96,66 @@ extension LibraryListController {
 
 extension LibraryListController {
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return viewModel.count
+    switch section {
+      case 0: return libraryItems.count
+      default: fatalError("unexpected section \(section)")
+    }
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    switch viewModel[indexPath.row] {
-      case let .placeholder(metadata):
-        let cell: PlaceholderTableCell = tableView.dequeueReusableCell(for: indexPath)
-        cell.configure(for: metadata)
-        return cell
-      case let .selectLibrary(metadata):
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ExistingLibraryTableCell", for: indexPath)
-        cell.textLabel!.text = metadata.name
-        return cell
-      case .addLibraryAction:
-        return tableView.dequeueReusableCell(for: indexPath) as AddNewLibraryTableCell
+    switch indexPath.section {
+      case 0:
+        switch libraryItems[indexPath.row] {
+          case let .placeholder(metadata):
+            let cell: PlaceholderTableCell = tableView.dequeueReusableCell(for: indexPath)
+            cell.configure(for: metadata)
+            return cell
+          case let .selectLibrary(metadata):
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ExistingLibraryTableCell", for: indexPath)
+            cell.textLabel!.text = metadata.name
+            return cell
+          case .addLibraryAction:
+            return tableView.dequeueReusableCell(for: indexPath) as AddNewLibraryTableCell
+        }
+      default:
+        fatalError("unexpected section \(indexPath.section)")
     }
   }
 
   override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-    switch viewModel[indexPath.row] {
-      case .placeholder: return nil
-      case .selectLibrary, .addLibraryAction: return indexPath
+    switch indexPath.section {
+      case 0:
+        switch libraryItems[indexPath.row] {
+          case .placeholder: return nil
+          case .selectLibrary, .addLibraryAction: return indexPath
+        }
+      default:
+        fatalError("unexpected section \(indexPath.section)")
     }
   }
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    switch viewModel[indexPath.row] {
-      case .placeholder:
-        fatalError("should not be selectable")
-      case let .selectLibrary(metadata):
-        onSelection?(metadata)
-      case .addLibraryAction:
-        onAddLibraryButtonTap?()
-        tableView.deselectRow(at: indexPath, animated: true)
+    switch indexPath.section {
+      case 0:
+        switch libraryItems[indexPath.row] {
+          case .placeholder:
+            fatalError("should not be selectable")
+          case let .selectLibrary(metadata):
+            onSelection?(metadata)
+          case .addLibraryAction:
+            onAddLibraryButtonTap?()
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+      default:
+        fatalError("unexpected section \(indexPath.section)")
     }
   }
 
   override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    return NSLocalizedString("libraryList.libraries", comment: "")
+    switch section {
+      case 0: return NSLocalizedString("libraryList.libraries", comment: "")
+      default: fatalError("unexpected section \(section)")
+    }
   }
 }
 
