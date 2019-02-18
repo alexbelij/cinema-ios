d4ee657dae5a7145fd645e033ad52b8361138c64
Martin Bauer <bauer_martin@icloud.com>
2019-02-18 16:12:20 +0100

    Switch Crashlytics destination based on configuration

--- upload-symbols.sh
+++ upload-symbols.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 if [[ -z $1 ]]; then
-  echo "missing required dSYMs directory"
-else
-  dSYMsDir=$1
-  Pods/Fabric/upload-symbols -gsp CinemaKit/Assets/GoogleService-Info.plist -p ios $dSYMsDir
+  echo "missing required configuration"
+  exit 1
 fi
+configuration=$1
+
+if [[ -z $2 ]]; then
+  echo "missing required dSYMs directory"
+  exit 2
+fi
+dSYMsDir=$2
+
+Pods/Fabric/upload-symbols -gsp "CinemaKit/Assets/GoogleService-Info-$configuration.plist" -p ios $dSYMsDir
